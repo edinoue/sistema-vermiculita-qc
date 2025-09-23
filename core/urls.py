eaaa5eae0eefd78d6016c55ff0548efc34c9134d
edinoue <91337810+edinoue@users.noreply.github.com>
2025-09-23 10:30:55 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,10 @@
-"""
-URLs do app core
-"""
-
 from django.urls import path
 from . import views
 
 app_name = 'core'
 
 urlpatterns = [
-    # Views principais
     path('', views.HomeView.as_view(), name='home'),
     path('login/', views.LoginView.as_view(), name='login'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
-    path('dashboard/', views.DashboardView.as_view(), name='dashboard'),
+    path('logout/', views.logout_view, name='logout'),
 ]
